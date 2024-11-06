87ab4394d54005a06937be65c63017483fc1e07f
Neolux Lee <neolux_lee@outlook.com>
2024-11-06 22:12:38 +0800

    try to find if running on cloud server and use another sql config

--- flaskr/config.py
+++ flaskr/config.py
@@ -27,7 +27,7 @@ def am_i_on_cloud():
 class Config:
     # 应用开放的主机和端口
     APP_HOST = '0.0.0.0'
-    APP_PORT = 5999
+    APP_PORT = 5000
 
     SECRET_KEY = 'ilovecandy'
 
@@ -36,6 +36,8 @@ class Config:
 
     # 服务器上使用的数据库配置
     if am_i_on_cloud():
+        print('I am on cloud.')
+        APP_PORT = 4917
         SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://m11154_cieusche:Qiushi2024@mysql8.serv00.com/m11154_CieuscheMgr'
 
     # 禁用追踪数据库的修改
